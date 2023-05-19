00bdadfd90e4972a94eb530c8e4b02ea1d0aa284
Arkadiy <shervard99@gmail.com>
2023-05-19 17:01:47 +0200

    Docstrings +
    Added docstrings
    version 0.0.1

--- functions.py
+++ functions.py
@@ -1,7 +1,15 @@
 from datetime import datetime,timedelta
 from collections import defaultdict
 
-def list_sort_by_date(lst):
+def list_sort_by_date(lst: list) -> list:
+    """Sorts list of dictionaries by dict["date"] value
+
+    Args:
+        lst (list): list of dictionaries with "date" key
+
+    Returns:
+        list: sorted list
+    """
     
     todays_date = datetime.now()
     
@@ -17,18 +25,35 @@ def list_sort_by_date(lst):
     result = sorted(lst, key = sort_funk)
     return result
 
-def is_leap_year(year):
+def is_leap_year(year: int) -> bool:
+    """Chek if year is leap or not
+
+    Args:
+        year (int): year format YYYY
+
+    Returns:
+        bool: True if year is leap otherwise False
+    """
+    
     return year%4 == 0 and (year%100 != 0 or year%400 == 0)
 
-def next_week_birthdays(users: list) -> str:
+def next_week_birthdays(members: list) -> str:
+    """ Find all members that have birthday in the next week from today
+
+    Args:
+        users (list): list of members dictionary with "name" and "date" keys
+
+    Returns:
+        str: <week_day>: <member_names>
+    """
     
     week_birthdays = defaultdict(list)
     todays_date = datetime.now()
     next_week_date = todays_date + timedelta(weeks=1)
     
-    users = list_sort_by_date(users)
+    members = list_sort_by_date(members)
     
-    for user in users:
+    for user in members:
         
         _,month,date = list(map(int, user['date'].split("-")))
         
